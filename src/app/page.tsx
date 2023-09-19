a39bbdbbd9726846836e5bac42caf9a4f9a85a20
Matt523 <matthewralphlee@gmail.com>
2023-09-19 14:10:47 +0900

    designed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,17 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center h-screen p-8">
-        <div>
-          <h1 className="text-9xl">To Do App</h1>
-          <h2 className="mx-48 my-12 text-4xl">
+        <div className="px-8">
+          <h1 className="text-7xl text-white font-bold">Task Charger</h1>
+          <div
+            style={{
+              backgroundImage:
+                "radial-gradient(farthest-corner at 10% 120%, #7140FF 0%, #6E00C3 32.98%, #E129FF 100%)",
+            }}
+            className="font-bold text-xl bg-clip-text text-transparent mt-12"
+          >
             Organize your task and clean up
-          </h2>
+          </div>
         </div>
       </div>
     </main>
